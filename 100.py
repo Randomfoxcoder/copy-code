4e90ac5161cfb6b4e3a1e096dddd2d84f24652a1
Randomfoxcoder <145066487+Randomfoxcoder@users.noreply.github.com>
2023-09-14 16:39:19 -0400

    Add files via upload

---
+++ 100.py
@@ -0,0 +1,12 @@
+# initialization
+count = 1
+
+# exit limit
+n = int(input("How many times to print:\n"))+ int(1)
+#Text input
+Text = input ('what to print:\n')
+#while loop
+while count in range(n):
+    print(str (count) + " " + str(Text))
+    
+    count += 1
